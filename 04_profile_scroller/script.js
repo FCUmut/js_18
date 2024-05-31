f8ceafdde068392f638f306189b27d1443a06dda
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-05-31 23:44:54 +0300

    commit
    commit

--- 04_profile_scroller/script.js
+++ 04_profile_scroller/script.js
@@ -1,34 +1,62 @@
 const people = [
   {
-    name: 'Jamie Williams',
+    name: "Jamie Williams",
     age: 26,
-    gender: 'female',
-    location: 'Los Angeles, CA',
-    imageURL: 'https://randomuser.me/api/portraits/women/1.jpg',
-    looking: 'Female looking for male',
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
+    looking: "Female looking for male",
   },
   {
-    name: 'John Smith',
+    name: "John Smith",
     age: 35,
-    gender: 'male',
-    location: 'New York, NY',
-    imageURL: 'https://randomuser.me/api/portraits/men/1.jpg',
-    looking: 'Male looking for female',
+    gender: "male",
+    location: "New York, NY",
+    imageURL: "https://randomuser.me/api/portraits/men/1.jpg",
+    looking: "Male looking for female",
   },
   {
-    name: 'Bob Johnson',
+    name: "Bob Johnson",
     age: 42,
-    gender: 'male',
-    location: 'Chicago, IL',
-    imageURL: 'https://randomuser.me/api/portraits/men/2.jpg',
-    looking: 'Male looking for male',
+    gender: "male",
+    location: "Chicago, IL",
+    imageURL: "https://randomuser.me/api/portraits/men/2.jpg",
+    looking: "Male looking for male",
   },
   {
-    name: 'Shannon Jackson',
+    name: "Shannon Jackson",
     age: 29,
-    gender: 'female',
-    location: 'Los Angeles, CA',
-    imageURL: 'https://randomuser.me/api/portraits/women/2.jpg',
-    looking: 'Female looking for female',
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
+    looking: "Female looking for female",
   },
 ];
+
+const container = document.querySelector(".container");
+const img = document.querySelector("img");
+const profileInfo = document.querySelector(".profile-info");
+const nextBtn = document.querySelector("#next");
+
+function* createPeopleIterator() {
+  let index = 0;
+  while (true) {
+    yield people[index++ % people.length]; // with this way it can loop in array infinitely
+  }
+}
+
+const iterator = createPeopleIterator();
+
+nextBtn.addEventListener("click", () => {
+  const person = iterator.next().value;
+
+  img.src = person.imageURL; // in DOM you can change img source with .src
+  profileInfo.querySelector("h3").textContent = person.name;
+  profileInfo.querySelectorAll("p")[0].textContent = `${person.age} years old`;
+  profileInfo.querySelectorAll(
+    "p"
+  )[1].textContent = `From ${person.age} location`;
+  profileInfo.querySelectorAll("p")[2].textContent = person.looking;
+});
+
+nextBtn.click();
